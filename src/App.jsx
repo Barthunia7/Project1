1af6c784ddc4ecbd8df6078f52deb2ac6f37f5eb
Kiran <sainikiran7852@gmail.com>
2026-05-23 17:23:56 +0530

    Initial Project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow">
+        Speech-To-Text App Live!
+      </h1>
+    </div>
+  );
+}
+export default App;
